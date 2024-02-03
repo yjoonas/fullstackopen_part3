c289ddb7850ac052ac63b2af75d7447ab1814d31
joonas <joonas.yli-saari@fidera.fi>
2024-02-03 21:21:05 +0200

    added env support fort port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan')
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 morgan.token('body', (req) => {
     return JSON.stringify(req.body)
